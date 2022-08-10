6b0ae7c276972c5e2c8e0eac1ae76eb13006d483
Federico Etchevest <federico.etchevest@gmail.com>
2022-08-09 21:30:21 -0300

    Incorporando librerias

--- etchevest-clase4-cotizador/eventos.js
+++ etchevest-clase4-cotizador/eventos.js
@@ -11,17 +11,30 @@ function cotizar (){
         error.innerHTML = `<div class=" container mt-3 p-3 mb-2 bg-danger text-white"><h2> Seleccione una opción </h2></div>`;        
     }
 
+    function desaparecerError(){
+        let desaparecerError = document.querySelector ("#resultado")
+        desaparecerError.innerHTML = "<h2></h2>"
+    }
+
     if(marca==="Ford"){
         if(planBasico.checked){
-            let resultado1 = document.querySelector("#resultado")
-            resultado1.innerHTML = `<h2> El valor de tu seguro es de $${precioBasico} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioBasico} por mes`,
+                'success'
+              )
+            desaparecerError()
         } else if(planBasico = "||"){
             marcarError()
         }
 
         if(planCompleto.checked){
-            let resultado2 = document.querySelector("#resultado")
-            resultado2.innerHTML = `<h2> El valor de tu seguro es de $${precioCompleto} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioCompleto} por mes`,
+                'success'
+              )
+            desaparecerError()
         }
     }else{
         marcarError()
@@ -29,24 +42,41 @@ function cotizar (){
 
     if(marca === "Chevrolet"){
         if(planBasico.checked){
-            let resultado3 = document.querySelector("#resultado")
-            resultado3.innerHTML = `<h2> El valor de tu seguro es de $${precioBasico *= 1.5} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioBasico *= 1.5} por mes`,
+                'success'
+              )
+            desaparecerError()
         }
         if(planCompleto.checked){
-            let resultado4 = document.querySelector("#resultado")
-            resultado4.innerHTML = `<h2> El valor de tu seguro es de $${precioCompleto *= 1.5} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioCompleto *= 1.5} por mes`,
+                'success'
+              )
+            desaparecerError()
         }
         
     }
 
     if(marca==="Renault"){
         if(planBasico.checked){
-            let resultado5 = document.querySelector("#resultado")
-            resultado5.innerHTML = `<h2> El valor de tu seguro es de $${precioBasico *= 2} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioBasico *= 2} por mes`,
+                'success'
+              )
+            desaparecerError()
         }
         if(planCompleto.checked){
-            let resultado6 = document.querySelector("#resultado")
-            resultado6.innerHTML = `<h2> El valor de tu seguro es de $${precioCompleto *= 2} por mes </h2>`
+            Swal.fire(
+                'Muchas gracias por elegirnos!',
+                `el valor de su seguro es de $${precioBasico *= 2} por mes`,
+                'success'
+              )
+            desaparecerError()
         }
-    } 
+    }
+    
 }
